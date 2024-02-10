e951f6306a4c047ac1c33007fe98ebc5ea5aaa2b
zozo404 <enzocuny66@gmail.com>
2024-02-10 19:05:32 +0100

    📱 ✅ | Home page
    Refactor navigation and layout classes in Header.vue and Propos.vue
    
    Updates navigation and layout classes in Header and Propos components
    for improved responsiveness and consistency. Removes unnecessary
    classes, adjusts padding, and uses more appropriate breakpoints for
    better alignment. Also, includes a full-screen image feature with a
    pop-up modal for a better user experience. No issues related.

--- components/Propos.vue
+++ components/Propos.vue
@@ -1,6 +1,31 @@
 <template>
-  <section>
-    <div class="italic leading-8 px-4 text-[15px] text-justify pb-14 text-base SamsungGA425G:px-8">
+  <section class="md:flex items-center">
+    <div
+      class="hidden text-center md:flex items-center flex-col fade-scroll md:w-1/2"
+      :class="{ visible: isVisible }"
+    >
+      <NuxtImg
+        :src="CartesPostales.imageId.asset._ref"
+        provider="sanity"
+        :alt="CartesPostales.imageId.alt"
+        class="w-full shadow-none transition1sec shadow-right-bottom sm:w-4/5 md:w-[70%] cursor-pointer"
+        @click="showFullScreenImage(CartesPostales.imageId.asset._ref)"
+      />
+      <h1 class="pt-2">{{ CartesPostales.name }}</h1>
+      <!-- image pop-up -->
+      <div v-if="isFullScreen" class="full-screen-image flex-col">
+        <span class="close" @click="isFullScreen = false">&times;</span>
+        <NuxtImg
+          provider="sanity"
+          class="modal-content"
+          :src="CartesPostales.imageId.asset._ref"
+        />
+        <p class="text-white">{{ CartesPostales.name }}</p>
+      </div>
+    </div>
+    <div
+      class="italic leading-8 px-4 text-[15px] text-justify pb-14 text-base SamsungGA425G:px-8 md:w-1/2"
+    >
       <p>
         A l’aube de la retraite, je me suis inscrit dans un club de dessin avec
         un professeur de bon niveau et la petite flamme presque complètement
@@ -24,6 +49,16 @@
 </template>
 
 <script>
-export default {};
+import scrollFadeMixin from "~/mixins/scrollFadeMixin";
+import { mapGetters } from "vuex";
+export default {
+  mixins: [scrollFadeMixin],
+  name: "Presentation",
+  computed: {
+    ...mapGetters(["getCartesPostales"]),
+    CartesPostales() {
+      return this.getCartesPostales.find((el) => el.name === "Le Castillet");
+    },
+  },
+};
 </script>
-
