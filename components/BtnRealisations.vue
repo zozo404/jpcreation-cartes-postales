21380b7028bd6964a7cb0eb3543b5430068cd9b4
zozo404 <enzocuny66@gmail.com>
2024-02-14 15:31:54 +0100

    💄 |pt btn realisations

--- components/BtnRealisations.vue
+++ components/BtnRealisations.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-center">
+  <div class="flex justify-center pt-12">
     <NuxtLink to="mes-realisations" class="bg-orange-400 px-4 py-2 text-xl transition1sec hover:bg-cyan-500 rounded-md">Toutes mes réalisations</NuxtLink>
   </div>
 </template>
